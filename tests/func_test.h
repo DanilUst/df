2ed365cb928798fc1e265f25f0de148ee94a21e1
DanilUst <78728660+DanilUst@users.noreply.github.com>
2021-03-22 21:59:18 +0300

    Update func_test.h

--- tests/func_test.h
+++ tests/func_test.h
@@ -51,6 +51,11 @@ TEST(Func, test10_negative){
     EXPECT_EQ(0, res.x2);
 
 }
+TEST(Func, test11_negative){
+    solution res = Func(1, 0, 0);
+    EXPECT_EQ(1, res.count);
+    EXPECT_EQ(0, res.x1);
+}
 
 
 
